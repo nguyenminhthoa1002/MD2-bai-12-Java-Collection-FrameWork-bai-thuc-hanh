7711a3554e4fbe77c48a309d555b504b1744e563
nguyenminhthoa1002 <minhthoa100299@gmail.com>
2022-10-12 08:56:46 +0700

    first commit

---
+++ src/thucHanh4/TestBST.java
@@ -0,0 +1,17 @@
+package thucHanh4;
+
+public class TestBST {
+    public static void main(String[] args) {
+        BTS<String> tree = new BTS<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        System.out.println("Inorder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
